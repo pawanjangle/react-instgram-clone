031b1198ad74f0a6c1f10c300c9e571fe3a189e1
Pawan <pawanjangle1000@gmail.com>
2021-01-08 08:49:02 +0530

    package.json changed

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -1,5 +1,4 @@
 const jwt = require( 'jsonwebtoken' );
-const { jwtSecret } = require( '../config/keys' );
 const User = require("../models/user");
 module.exports = async ( req, res, next ) =>{
 const { authorization } = req.headers;
@@ -7,7 +6,7 @@ if( !authorization ){
 return res.status( 422 ).json( {error: "You must be logged in1"} )
 }
 const token = authorization.replace( "Bearer ", "" );
-const verified = jwt.verify( token, jwtSecret);
+const verified = jwt.verify( token, process.env.jwtSecret);
 if( !verified ){
 return res.status( 422 ).json( { error: "You must be logged in2" } )
 }
